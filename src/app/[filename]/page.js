b2c0f939793d1159b8803229483c8688bbbdb507
kayne-lee <kayne.lee2@outlook.com>
2024-02-21 00:59:16 -0500

    hide .env

--- src/app/[filename]/page.js
+++ src/app/[filename]/page.js
@@ -2,11 +2,12 @@
 import axios from "axios";
 import { useEffect } from "react";
 
+
 export default function FilePage({params}) {
     const filename = params.filename;
     useEffect(() => {
         axios.get('/api/transcribe?filename='+filename);
-    }, [filename])
+    }, [filename]);
     return (
         <div>{filename}</div>
     )
--- src/app/api/transcribe/route.js
+++ src/app/api/transcribe/route.js
@@ -1,57 +1,28 @@
-import { GetTranscriptionJobCommand, StartTranscriptionJobCommand, TranscribeClient } from "@aws-sdk/client-transcribe";
+import { StartTranscriptionJobCommand, TranscribeClient } from "@aws-sdk/client-transcribe";
 
-function getClient() {
-    return new TranscribeClient({
+export async function GET(req) {
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.searchParams);
+    const filename = searchParams.get('filename');
+
+    const transcribeClient = new TranscribeClient({
         region: 'ca-central-1',
         credentials: {
             accessKeyId: process.env.AWS_ACCESS_KEY,
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         },
     });
-}
 
-function createTranscriptCommand(filename) {
-    return new StartTranscriptionJobCommand({
+    const transcriptionCommand = new StartTranscriptionJobCommand({
         TranscriptionJobName: filename,
         OutputBucketName: process.env.BUCKET_NAME,
         OutputKey: filename + '.transcription',
         IdentifyLanguage: true,
         Media: {
-            MediaFileUrl: 's3://' + process.env.BUCKET_NAME + '/' + filename,
-        },
-    });
-}
-export async function GET(req) {
-    const url = new URL(req.url);
-    const searchParams = new URLSearchParams(url.searchParams);
-    const filename = searchParams.get('filename')
+            MediaFileUri: 's3://' + process.env.BUCKET_NAME + '/' + filename,
+        }
+    })
 
-    const transcribeClient = getClient();
-
-    const transcriptionStatus = new GetTranscriptionJobCommand({
-        TranscriptionJobName: filename,
-    });
-
-    let exitstingJobFound = false;
-    try {
-        const jobStatusResult = await transcribeClient.send(
-            transcriptionStatus
-        );
-        exitstingJobFound = true;
-    } catch (e) {
-
-    }
-
-    console.log(jobStatusResult);
-    
-    if (!exitstingJobFound) {
-        const transcriptionCommand = StartTranscriptionJobCommand(filename);
-        const result = await transcribeClient.send(transcriptionCommand);
-    }
-
-    
-    return Response.json('ok')
-    
-
-
+    const result = await transcribeClient.send(transcriptionCommand);
+    return Response.json(result);
 }
