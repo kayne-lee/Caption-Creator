bc246a7a038123410c817a0f666ce7e79c3b5ee7
kayne-lee <kayne.lee2@outlook.com>
2024-03-24 20:42:24 -0400

    Captions editor and preview

--- src/app/[filename]/page.js
+++ src/app/[filename]/page.js
@@ -1,18 +1,24 @@
 'use client'
+import { clearTranscriptionItems } from "@/libs/awsTranscriptionHelpers";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import TranscriptionItem from "../components/TranscriptionItem";
+import SparklesIcons from "../components/SparklesIcon";
 
 
 export default function FilePage({params}) {
     const filename = params.filename;
     const [isTranscribing, setIsTranscribing] = useState(false);
+    const [isFetchingInfo, setIsFetchingInfo] = useState(false);
     const [awsTranscriptionItems, setAwsTranscriptionItems] = useState([]);
     useEffect(() => {
         getTranscription();
     }, [filename]);
 
     function getTranscription() {
+        setIsFetchingInfo(true);
         axios.get('/api/transcribe?filename='+filename).then(response => {
+            setIsFetchingInfo(false);
             const status = response.data?.status;
             const transcription = response.data?.transcription;
             if(status === 'IN_PROGRESS') {
@@ -20,26 +26,54 @@ export default function FilePage({params}) {
                 setTimeout(getTranscription, 3000);
             } else {
                 setIsTranscribing(false);
-                console.log(transcription);
-                setAwsTranscriptionItems(transcription.results.items)
+                setAwsTranscriptionItems(clearTranscriptionItems(transcription.results.items))
             }
         });
     }
+    if (isTranscribing) {
+        return (
+            <div>Transcribing your video...</div>
+        );
+    }
+    if (isFetchingInfo) {
+        return (
+            <div>
+                Fetching information...
+            </div>
+        )
+    }
     return (
         <div>
-            {filename}
-            <div>is transcribing: {JSON.stringify(isTranscribing)}</div>
-            {awsTranscriptionItems.length > 0 && awsTranscriptionItems.map(item => (
-                <div>
-                    <span className="text-white/50 mr-2">
-                        {item.start_time} - {item.end_time}
-                    </span>
-                    <span>
-                        {item.alternatives[0].content}
-                    </span>
-                    
+            <div className="grid grid-cols-2 gap-16">
+                <div className="">
+                    <h2 className="text-2xl mb-4 text-white/80">Transcription</h2>
+                    <div className="grid grid-cols-3 gap-1 sticky top-0 bg-blue-800/80 p-2 rounded-md">
+                        <div>From</div>
+                        <div>End</div>
+                        <div>Content</div>
+                    </div>
+                    {awsTranscriptionItems.length > 0 && awsTranscriptionItems.map(item => (
+                        <TranscriptionItem item={item} />
+                ))}
                 </div>
-            ))}
+                <div>
+                    <h2 className="text-2xl mb-4 text-white/80">Result</h2>
+                    <div className="mb-4 ">
+                        <button className="inline-flex gap-2 bg-green-600 py-2 px-6 rounded-full cursor-pointer">
+                            <SparklesIcons />
+                            <span>Apply captions</span>
+                        </button>
+                    </div>
+                        <div className="rounded-xl overflow-hidden">
+                            <video 
+                                controls
+                                src={"https://caption-creator.s3.amazonaws.com/"+filename}
+                            ></video>
+                        </div>
+                </div>
+            </div>
+            
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'bg-gradient-from': '#9747FF',
-        'bg-gradient-to': '#7D4BFF',
+        'bg-gradient-from': '#3370d4',
+        'bg-gradient-to': '#0c49ab',
       }
     },
   },
